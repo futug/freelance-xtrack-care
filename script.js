f8f255474a7d64fe730142346a424e0089a9ccf6
futug <102650930+futug@users.noreply.github.com>
2023-05-23 17:57:20 +0600

    smooth scroller

--- script.js
+++ script.js
@@ -31,3 +31,17 @@ submitsBtn.forEach((sub) => {
 secondPopUp.addEventListener("click", () => {
     secondPopUp.classList.remove("visible");
 });
+
+// POPUPS
+// SMOOTTH SCROLL
+const anchors = document.querySelectorAll('a[href*="#"]');
+for (let anchor of anchors) {
+    anchor.addEventListener("click", (e) => {
+        e.preventDefault();
+        const blockID = anchor.getAttribute("href");
+        document.querySelector("" + blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+        });
+    });
+}
